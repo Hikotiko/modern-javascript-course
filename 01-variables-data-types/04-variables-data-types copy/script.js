06a798c771f5c24589960fcf20388c963cd63290
Hikotiko <hikmet.demirci@gmail.com>
2025-05-15 19:43:23 +0200

    created a new branch

--- 01-variables-data-types/04-variables-data-types copy/script.js
+++ 01-variables-data-types/04-variables-data-types copy/script.js
@@ -1,72 +1,17 @@
-// Ways to declare variables
-// var, let, const
+// Primitive Data Types
+// String - sequence of charachters. Must be in quotes or backticks
+// Number - Integers as wel as floating-points numbers
+// Boolean - Logical entity / true or false
+// Null - Intentional absence of any object value
+// Undefined - A variable has not yet been defined / assigned
+// Symbol - Built-in object whose constuctor returns a unique symbol
+// BigInt - Numbers that are greate than the "number" type
 
-let firstName = 'Hikmet'
-let lastName = 'Demirci'
-
-console.log(firstName, lastName);
-
-let age = 30;
-
-console.log(age);
-
-// Naming Conventions
-// - Only letters, numbers, underscores and dollar signs
-// - Can't start with a number
-
-// Multi-Word Formatting
-// firstName = camelCase
-// first_name = underscore
-// FirstName = PascalCase
-// firstname = lowercase
-
-// Re-assigning Variables
-age = 31;
-
-console.log(age);
-
-// Initialise without assigning
-let score;
-
-score = 1;
-
-console.log(score);
-
-if (true) {
-   score = score +1
-}
-
-console.log(score);
-
-// const can't be re-assigned
-const x = 100;
-
-const arr = [1,2,3,4]
+// Reference Types (objects)
+// Reference types or "objecst" are a non-primitive value and when assigned
+// to a variable, the variable is given a reference to that value.
+//
+// Object literals, arrays, and function are all reference types
 
 
-// cannot be re-assigned
-//arr = [1,2,3,4,5]
 
-// arr has methods which can alter the value
-
-arr.push(5)
-console.log(arr);
-
-// attributes of const variables can be reassigned
-const person = {
-   name: 'Brad'
-};
-
-person.name = 'John'
-person.email = 'john@gemail.com'
-console.log(person);
-
-// declare multiple variables
-
-let a,b,c;
-
-console.log(a);   //result will be undefined
-
-a = 10
-
-console.log(a); // result will be 10
