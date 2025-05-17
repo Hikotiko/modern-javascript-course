c20cafd8715926e839d2f5652f2e44a0fea023c5
hikotiko <hikmet.demirci@gmail.com>
2025-05-17 17:10:06 +0200

    continue from type coersion lecture

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,22 @@
+// 1. let amount = "100";
+// 2. let amount = 100;
+// 3. let amount = '99.5';
+let amount = 0;
+
+// 1. Convert string to number
+
+// amount = parseInt(amount)
+// amount = +amount;
+// amount = Number(amount)
+
+// 2. Convert number to string
+// amount = amount.toString();
+// amount = String(amount)
+
+// 3. Convert string to decimal
+// amount = parseFloat(amount);
+
+// 4. Convert number to boolean
+amount = Boolean(amount);
+
+console.log(amount, typeof amount);
---
+++ 01-variables-data-types/08-type-coersion/script.js
@@ -0,0 +1,35 @@
+// 1. Arithmetic Operators
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// 2. Concatenation
+x = 'hello' + ' ' + 'world';
+
+// 3. Exponent
+x = 2 ** 3;
+
+// 4. Increment
+x = 1;
+x++;
+
+// 5. Decrement
+x--;
+
+// 6. Assigment operators
+x = 10;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x **= 5;
+x %= 5;
+
+// 7. Comparison operator
+x = 2 == 4;
+
+console.log(x);
